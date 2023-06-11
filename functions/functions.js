c1787b592cfe6abfea3557661bcf808c433db3f2
sabari-pt4974 <sabaribalaji.es@zohocorp.com>
2023-06-11 12:16:40 +0530

    functions exercise

---
+++ functions/functions.js
@@ -0,0 +1,20 @@
+function square()
+{
+    var square = [];
+    var numbers = [1,2,3,4,5];
+    for(const number of numbers)
+    {
+        square.push(number * number);
+    }
+    alert(square);
+}
+
+var numbers = [1,2,3,4,5];
+var double = () => {
+    var double = [];
+    for(number of numbers)
+    {
+        double.push(number * 2);
+    }
+    alert(double);
+}
