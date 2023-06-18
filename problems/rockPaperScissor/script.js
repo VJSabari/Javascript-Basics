86e6c28e76583f276534362f5b199b25890c805b
sabari-pt4974 <sabaribalaji.es@zohocorp.com>
2023-06-18 18:21:05 +0530

    optimized the js

--- problems/rockPaperScissor/script.js
+++ problems/rockPaperScissor/script.js
@@ -1,34 +1,42 @@
-var rockDiv = document.getElementById("rock")
-var paper = document.getElementById("paper")
-var scissor = document.getElementById("scissor")
 var stopDiv = document.getElementsByClassName("stop")
 var resultDiv = document.getElementById("result")
 var scoreDiv = document.getElementById("score")
 var score = 0
+var inputs = document.querySelectorAll(".child");
+var options = ["rock", "paper", "scissor"]
 
-function playGame(userInput){
-    var options = ["rock", "paper", "scissor"]
-    var random = options[Number(Math.floor(Math.random() * 3))]
-    console.log(random)
-    if(random == userInput)
-    {
-        resultDiv.innerHTML = "Match is drawn";
-        scoreDiv.innerHTML = "Your score is " + "<b>" + score + "</b>" ;
+
+
+inputs.forEach(input => {
+    input.onclick = () => {
+        
+        var userInput = input.getAttribute("value")
+        var random = options[Number(Math.floor(Math.random() * 3))]
+        console.log(random)
+        if(random == userInput)
+        {
+            resultDiv.innerHTML = "Match is drawn";
+            scoreDiv.innerHTML = "Your score is " + "<b>" + score + "</b>" ;
+        }
+        else if(userInput == "rock" && random == "paper" || userInput == "paper" && random == "scissor" || userInput == "scissor" && random == "rock")
+        {
+            score -= 1;
+            resultDiv.innerHTML = "You lose the match";
+            scoreDiv.innerHTML = "Your score is " + "<b>" + score + "</b>" ;
+        }
+        else{
+            score += 1;
+            resultDiv.innerHTML = "You win the match";
+            scoreDiv.innerHTML = "Your score is " + "<b>" + score + "</b>" ;
+        }
+        resultDiv.style.display ="block";
+        scoreDiv.style.display = "block";
     }
-    else if(userInput == "rock" && random == "paper" || userInput == "paper" && random == "scissor" || userInput == "scissor" && random == "rock")
-    {
-        score -= 1;
-        resultDiv.innerHTML = "You lose the match";
-        scoreDiv.innerHTML = "Your score is " + "<b>" + score + "</b>" ;
-    }
-    else{
-        score += 1;
-        resultDiv.innerHTML = "You win the match";
-        scoreDiv.innerHTML = "Your score is " + "<b>" + score + "</b>" ;
-    }
-    resultDiv.style.display ="block";
-    scoreDiv.style.display = "block";
 }
+)
+
+var stopBtn = document.querySelector(".stop")
+stopBtn.onclick = () => reset()
 
 function reset()
 {
